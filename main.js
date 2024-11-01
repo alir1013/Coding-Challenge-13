2d8163ed514c6f7d3ffd14d33eb5c793b3facf3a
Roan Ali <alir13@usf.edu>
2024-11-01 19:32:23 -0400

    Format product information dynamically

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Task 2:Fetch Products from the API Using Fetch and Promises
-
 const productContainer = document.getElementById("productContainer");
 
 // Fetching data from the API
@@ -14,7 +13,29 @@ fetch('https://www.course-api.com/javascript-store-products')
         displayProducts(products);
     })
     .catch(error => {
-        console.error("could not fetch data", error);
+        console.error("could not fetch data", error); 
     });
+
+//Task 3: Display Product Details Dynamically
+
+function displayProducts(products) {
+    products.forEach(product => {
+        const { name, company, price } = product.fields;
+        const image= product.fields.image[0].url;
+        const productPrice = (price / 100).toFixed(2);
+
+        const productElement = document.createElement("div");
+        productElement.classList.add("product");
+
+        productElement.innerHTML = `
+            <img src="${image}" alt="${name}" class="product-image" />
+            <h3 class="product-name">${name}</h3>
+            <p class="product-company">by ${company}</p>
+            <p class="product-price">$${productPrice}</p>
+        `;
+
+        productContainer.appendChild(productElement);
+    });
+};
 
 
